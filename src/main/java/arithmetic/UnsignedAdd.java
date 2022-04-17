7a9e05ece344b47096ea64626d50ed868403b2c8
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-04-17 12:25:49 -0400

    CIRCUIT: Updated Signed Add to have more standard convention of left to right bit reading. Updated Inverter Gate labeling.

--- src/main/java/arithmetic/UnsignedAdd.java
+++ src/main/java/arithmetic/UnsignedAdd.java
@@ -35,19 +35,19 @@ public class UnsignedAdd extends DigitalCircuit {
         // Overflow = C_n-1
         CircuitNode[] outputNodes = new CircuitNode[nBit+1];
 
-        for (int i = 0; i < nBit; i++) {
+        for (int i = nBit-1; i >= 0; i--) {
             outputNodes[i] = new CircuitNode(label + " Output_" + i);
 
-            carryAnds[i][0] = new AndGate(label + " CarryCalcAnd_" + i + "0", 2);
-            carryAnds[i][0].assignInput(0, i>0 ? carryOrs[i-1].getOutput(0) : GND);
+            carryAnds[i][0] = new AndGate(label + " CarryAnd_" + i + "_0", 2);
+            carryAnds[i][0].assignInput(0, i<nBit-1 ? carryOrs[i+1].getOutput(0) : GND);
             carryAnds[i][0].assignInput(1, getPortOutput(i));
 
-            carryAnds[i][1] = new AndGate(label + " CarryCalcAnd_" + i + "1", 2);
+            carryAnds[i][1] = new AndGate(label + " CarryAnd_" + i + "_1", 2);
             carryAnds[i][1].assignInput(0, getPortOutput(i));
             carryAnds[i][1].assignInput(1, getPortOutput(i+nBit));
 
-            carryAnds[i][2] = new AndGate(label + " CarryCalcAnd_" + i + "2", 2);
-            carryAnds[i][2].assignInput(0, i>0 ? carryOrs[i-1].getOutput(0) : GND);
+            carryAnds[i][2] = new AndGate(label + " CarryAnd_" + i + "_2", 2);
+            carryAnds[i][2].assignInput(0, i<nBit-1 ? carryOrs[i+1].getOutput(0) : GND);
             carryAnds[i][2].assignInput(1, getPortOutput(i+nBit));
 
             carryOrs[i] = new OrGate(label + " CarryOr_" + i, 3);
@@ -55,17 +55,17 @@ public class UnsignedAdd extends DigitalCircuit {
                 carryOrs[i].assignInput(j, carryAnds[i][j].getOutput(0));
             }
 
-            outputAnds[i] = new AndGate(label + " OutputCalcAnd_" + i, 3);
+            outputAnds[i] = new AndGate(label + " OutputAnd_" + i, 3);
             outputAnds[i].assignInput(0, getPortOutput(i));
             outputAnds[i].assignInput(1, getPortOutput(i+nBit));
-            outputAnds[i].assignInput(2, i>0 ? carryOrs[i-1].getOutput(0) : GND);
+            outputAnds[i].assignInput(2, i<nBit-1 ? carryOrs[i+1].getOutput(0) : GND);
 
             outputXors[i] = new XorGate(label + "OutputXor_" + i, 3);
             outputXors[i].assignInput(0, getPortOutput(i));
             outputXors[i].assignInput(1, getPortOutput(i+nBit));
-            outputXors[i].assignInput(2, i>0 ? carryOrs[i-1].getOutput(0) : GND);
+            outputXors[i].assignInput(2, i<nBit-1 ? carryOrs[i+1].getOutput(0) : GND);
 
-            outputOrs[i] = new OrGate(label + " OutputAnd_" + i, 2);
+            outputOrs[i] = new OrGate(label + " OutputOr_" + i, 2);
             outputOrs[i].assignInput(0, outputXors[i].getOutput(0));
             outputOrs[i].assignInput(1, outputAnds[i].getOutput(0));
 
@@ -75,10 +75,20 @@ public class UnsignedAdd extends DigitalCircuit {
         assignOutput(nBit, outputNodes[nBit]);
     }
 
+    public UnsignedAdd(String label, int nBit, CircuitNode[] outputs) {
+        this(label, nBit);
+        assignOutputs(outputs);
+    }
+
+    public UnsignedAdd(String label, int nBit, CircuitNode[] outputs, CircuitNode[] inputs) {
+        this(label, nBit, outputs);
+        assignInputs(inputs);
+    }
+
     @Override
     public void assignOutput(int i, @NotNull CircuitNode output) {
         if (i==getNumOutputs()-1) {
-            carryOrs[carryOrs.length-1].assignOutput(0, output);
+            carryOrs[0].assignOutput(0, output);
         } else if (i>=0 && i<getNumOutputs()-1) {
             outputOrs[i].assignOutput(0, output);
         }
@@ -88,7 +98,7 @@ public class UnsignedAdd extends DigitalCircuit {
     public void evaluate() {
         super.evaluate();
 
-        for (int i = 0; i < getNumOutputs()-1; i++) {
+        for (int i = getNumOutputs()-2; i >= 0; i--) {
             for (int j = 0; j < 3; j++) {
                 carryAnds[i][j].evaluate();
             }
--- src/test/java/arithmetic/UnsignedAddTest.java
+++ src/test/java/arithmetic/UnsignedAddTest.java
@@ -13,11 +13,16 @@ public class UnsignedAddTest {
     private UnsignedAdd unsignedAdd;
 
     private static final CircuitNode[] NO_OVERFLOW_4_BIT = { VDD, GND, GND, VDD, GND, GND, VDD, GND };
-
-    private static final CircuitNode[] OVERFLOW_4_BIT = { GND, GND, GND, VDD, GND, VDD, VDD, VDD };
+    private static final CircuitNode[] OVERFLOW_4_BIT = { VDD, GND, GND, GND, VDD, VDD, VDD, GND };
+    private static final CircuitNode[] ADD_ZEROES = { GND, GND, GND, GND, GND, GND, GND, GND };
+    private static final CircuitNode[] ADD_FFS = { VDD, VDD, VDD, VDD, VDD, VDD, VDD, VDD };
 
     private static final Boolean[] NO_OVERFLOW_4_BIT_ANS = {true, false, true, true, false};
     private static final Boolean[] OVERFLOW_4_BIT_ANS = {false, true, true, false, true};
+    private static final Boolean[] ADD_ZEROES_ANS = {false, false, false, false, false};
+    private static final Boolean[] ADD_FFS_ANS = {true, true, true, false, true};
+
+
 
     @BeforeEach
     void init() {
@@ -33,5 +38,13 @@ public class UnsignedAddTest {
         unsignedAdd.assignInputs(OVERFLOW_4_BIT);
         unsignedAdd.evaluate();
         assertArrayEquals(OVERFLOW_4_BIT_ANS, unsignedAdd.readOutputs(), "4 Bit Add With Overflow");
+
+        unsignedAdd.assignInputs(ADD_ZEROES);
+        unsignedAdd.evaluate();
+        assertArrayEquals(ADD_ZEROES_ANS, unsignedAdd.readOutputs(), "4 Bit Add Zeroes");
+
+        unsignedAdd.assignInputs(ADD_FFS);
+        unsignedAdd.evaluate();
+        assertArrayEquals(ADD_FFS_ANS, unsignedAdd.readOutputs(), "4 Bit Add 0xFF and 0xFF");
     }
 }
